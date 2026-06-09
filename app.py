36d15557d8231d1b29b977d96b9f37463372aa12
Arya Bhongade <aryabhongade15@gmail.com>
2026-06-09 22:57:24 +0530

    Add session management and dashboard protection

--- app.py
+++ app.py
@@ -1,22 +1,57 @@
-from flask import Flask, render_template
+from flask import (
+    Flask,
+    render_template,
+    session,
+    redirect,
+    url_for
+)
 
 app = Flask(__name__)
+app.secret_key = "face_login_secret_key"
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/register")
 def register():
     return render_template("register.html")
+
 
 @app.route("/login")
 def login():
     return render_template("login.html")
 
+
 @app.route("/dashboard")
 def dashboard():
-    return render_template("dashboard.html")
+
+    if "user" not in session:
+        return redirect(url_for("login"))
+
+    return render_template(
+        "dashboard.html",
+        username=session["user"]
+    )
+
+
+@app.route("/test-login")
+def test_login():
+
+    session["user"] = "Arya"
+
+    return redirect(url_for("dashboard"))
+
+
+@app.route("/logout")
+def logout():
+
+    session.pop("user", None)
+
+    return redirect(url_for("home"))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
